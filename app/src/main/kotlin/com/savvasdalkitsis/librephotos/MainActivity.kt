5c0983219d664b5ecf13fc6f241db6f0cc296f2e
Savvas Dalkitsis <kurosavvas@gmail.com>
2022-03-15 18:20:11 +0000

    Adding lazy staggered grid

--- app/src/main/kotlin/com/savvasdalkitsis/librephotos/MainActivity.kt
+++ app/src/main/kotlin/com/savvasdalkitsis/librephotos/MainActivity.kt
@@ -3,13 +3,21 @@ package com.savvasdalkitsis.librephotos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nesyou.staggeredgrid.LazyStaggeredGrid
+import com.nesyou.staggeredgrid.StaggeredCells
 import com.savvasdalkitsis.librephotos.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LazyStaggeredGrid(cells = StaggeredCells.Adaptive(minSize = 180.dp)) {
+                        items(60) {
+                            val random: Double = 100 + Math.random() * (500 - 100)
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_launcher_background),
+                                contentDescription = null,
+                                modifier = Modifier.height(random.dp).padding(10.dp),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                    }
                 }
             }
         }
