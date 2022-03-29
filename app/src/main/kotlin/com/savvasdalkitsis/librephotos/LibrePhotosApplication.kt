48d53bc4ddfa86f4d51b92b7920f46e7ae3f6542
Savvas Dalkitsis <kurosavvas@gmail.com>
2022-03-29 18:49:06 +0100

    Dependencies for work manager

--- app/src/main/kotlin/com/savvasdalkitsis/librephotos/LibrePhotosApplication.kt
+++ app/src/main/kotlin/com/savvasdalkitsis/librephotos/LibrePhotosApplication.kt
@@ -2,15 +2,24 @@ package com.savvasdalkitsis.librephotos
 
 import android.app.Application
 import android.webkit.WebView
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
-class LibrePhotosApplication : Application() {
+class LibrePhotosApplication : Application(), Configuration.Provider {
+
+    @Inject lateinit var workerFactory: HiltWorkerFactory
 
     override fun onCreate() {
         super.onCreate()
         WebView.setWebContentsDebuggingEnabled(true)
         Timber.plant(Timber.DebugTree())
     }
+
+    override fun getWorkManagerConfiguration() = Configuration.Builder()
+        .setWorkerFactory(workerFactory)
+        .build()
 }
