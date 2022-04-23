8959620b16c246c80b3c9d93335624e6c7dd1a5f
Savvas Dalkitsis <kurosavvas@gmail.com>
2022-04-23 18:58:56 +0100

    Making settings sections collapsible

--- settings/src/main/kotlin/com/savvasdalkitsis/librephotos/settings/view/SettingsGroup.kt
+++ settings/src/main/kotlin/com/savvasdalkitsis/librephotos/settings/view/SettingsGroup.kt
@@ -1,30 +1,64 @@
 package com.savvasdalkitsis.librephotos.settings.view
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.savvasdalkitsis.librephotos.icons.R
 
 @Composable
-fun SettingsGroup(title: String, content: @Composable ColumnScope.() -> Unit) {
+fun SettingsGroup(
+    title: String,
+    content: @Composable ColumnScope.() -> Unit,
+) {
     Surface(elevation = 4.dp) {
         Column {
-            Text(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth(),
-                text = title,
-                style = MaterialTheme.typography.h6,
-                textAlign = TextAlign.Center,
-            )
-            content()
+            val arrowAngle = remember { Animatable(180f) }
+            var collapsed by remember { mutableStateOf(false) }
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .clickable { collapsed = !collapsed }
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.Center),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                ) {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.h6,
+                    )
+                    Icon(
+                        modifier = Modifier.rotate(arrowAngle.value),
+                        painter = painterResource(id = R.drawable.ic_arrow_up),
+                        contentDescription = null,
+                    )
+                }
+            }
+            AnimatedVisibility(visible = !collapsed) {
+                Column {
+                    content()
+                }
+            }
+            LaunchedEffect(collapsed) {
+                arrowAngle.animateTo(
+                    when {
+                        collapsed -> 180f
+                        else -> 0f
+                    }
+                )
+            }
         }
     }
 }
