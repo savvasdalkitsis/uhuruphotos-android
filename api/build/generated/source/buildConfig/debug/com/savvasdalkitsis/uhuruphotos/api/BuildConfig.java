20ee5c66d07da5a9c2de25b5d34fed72d703befa
Savvas Dalkitsis <kurosavvas@gmail.com>
2022-05-28 03:58:18 +0100

    Creating api grouping submodule to reduce top level noise in the project

---
+++ api/build/generated/source/buildConfig/debug/com/savvasdalkitsis/uhuruphotos/api/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.savvasdalkitsis.uhuruphotos.api;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.savvasdalkitsis.uhuruphotos.api";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final int APP_VERSION_CODE = 21;
+  // Field from default config.
+  public static final String APP_VERSION_NAME = "0.0.28";
+}
