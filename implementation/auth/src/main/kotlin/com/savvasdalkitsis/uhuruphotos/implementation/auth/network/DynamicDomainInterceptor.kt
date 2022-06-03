778a978654b20355826d923385a4a6a5497d1773
Savvas Dalkitsis <kurosavvas@gmail.com>
2022-06-03 14:26:58 +0100

    Fixing auth implementation package

---
+++ implementation/auth/src/main/kotlin/com/savvasdalkitsis/uhuruphotos/implementation/auth/network/DynamicDomainInterceptor.kt
@@ -0,0 +1,48 @@
+/*
+Copyright 2022 Savvas Dalkitsis
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+package com.savvasdalkitsis.uhuruphotos.implementation.auth.network
+
+import com.savvasdalkitsis.uhuruphotos.api.auth.usecase.ServerUseCase
+import kotlinx.coroutines.runBlocking
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.io.IOException
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class DynamicDomainInterceptor @Inject constructor(
+    private val serverUseCase: ServerUseCase,
+) : Interceptor {
+
+    @Throws(Exception::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        return when (val serverUrl = runBlocking { serverUseCase.getServerUrl() }) {
+            null -> throw IOException("Server url is not initialised")
+            else -> chain.proceed(
+                request
+                    .newBuilder()
+                    .url(request.url.replace(serverUrl))
+                    .build()
+                )
+        }
+    }
+
+    private fun HttpUrl.replace(url: String) = toString()
+            .replace("https://localhost", url)
+}
---
+++ implementation/auth/src/main/kotlin/com/savvasdalkitsis/uhuruphotos/implementation/auth/weblogin/weblogin/navigation/WebLoginNavigationTarget.kt
@@ -0,0 +1,64 @@
+/*
+Copyright 2022 Savvas Dalkitsis
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+package com.savvasdalkitsis.uhuruphotos.implementation.auth.weblogin.weblogin.navigation
+
+import android.util.Base64
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import com.savvasdalkitsis.uhuruphotos.api.settings.usecase.SettingsUseCase
+import com.savvasdalkitsis.uhuruphotos.auth.weblogin.weblogin.seam.WebEffectsHandler
+import com.savvasdalkitsis.uhuruphotos.auth.weblogin.weblogin.seam.WebLoginAction
+import com.savvasdalkitsis.uhuruphotos.auth.weblogin.weblogin.seam.WebLoginEffect
+import com.savvasdalkitsis.uhuruphotos.auth.weblogin.weblogin.view.WebLogin
+import com.savvasdalkitsis.uhuruphotos.auth.weblogin.weblogin.view.WebLoginState
+import com.savvasdalkitsis.uhuruphotos.auth.weblogin.weblogin.viewmodel.WebLoginViewModel
+import com.savvasdalkitsis.uhuruphotos.api.navigation.NavigationTarget
+import com.savvasdalkitsis.uhuruphotos.api.navigation.navigationTarget
+import javax.inject.Inject
+
+class WebLoginNavigationTarget @Inject constructor(
+    private val effectsHandler: WebEffectsHandler,
+    private val settingsUseCase: SettingsUseCase,
+) : NavigationTarget {
+
+    override suspend fun NavGraphBuilder.create(navHostController: NavHostController) {
+        navigationTarget<WebLoginState, WebLoginEffect, WebLoginAction, WebLoginViewModel>(
+            name = name,
+            effects = effectsHandler,
+            themeMode = settingsUseCase.observeThemeModeState(),
+            initializer = { navBackStackEntry, actions ->
+                actions(WebLoginAction.LoadPage(navBackStackEntry.url))
+            },
+            createModel = { hiltViewModel() }
+        ) { state, _ ->
+            WebLogin(state)
+        }
+    }
+
+    companion object {
+        private const val name = "web-login/{url}"
+        fun name(url: String) = name.replace(
+            "{url}", Base64.encodeToString(url.toByteArray(), Base64.URL_SAFE)
+        )
+        private val NavBackStackEntry.url : String get() {
+            val encodedUrl = arguments!!.getString("url")!!
+            val url = Base64.decode(encodedUrl, Base64.URL_SAFE)
+            return String(url)
+        }
+    }
+}
