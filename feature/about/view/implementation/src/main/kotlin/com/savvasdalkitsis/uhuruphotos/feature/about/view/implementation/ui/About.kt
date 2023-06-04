dd5f465f78b6c44db24ffe0a61d3461d530febae
Savvas Dalkitsis <kurosavvas@gmail.com>
2023-06-04 13:26:05 +0100

    Adding scrollbar to about screen

--- feature/about/view/implementation/src/main/kotlin/com/savvasdalkitsis/uhuruphotos/feature/about/view/implementation/ui/About.kt
+++ feature/about/view/implementation/src/main/kotlin/com/savvasdalkitsis/uhuruphotos/feature/about/view/implementation/ui/About.kt
@@ -15,17 +15,25 @@ limitations under the License.
  */
 package com.savvasdalkitsis.uhuruphotos.feature.about.view.implementation.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.mikepenz.aboutlibraries.ui.compose.LibrariesContainer
 import com.savvasdalkitsis.uhuruphotos.feature.about.view.implementation.seam.actions.AboutAction
 import com.savvasdalkitsis.uhuruphotos.feature.about.view.implementation.seam.actions.NavigateBack
 import com.savvasdalkitsis.uhuruphotos.foundation.strings.api.R.string
 import com.savvasdalkitsis.uhuruphotos.foundation.ui.api.ui.BackNavButton
 import com.savvasdalkitsis.uhuruphotos.foundation.ui.api.ui.CommonScaffold
+import my.nanihadesuka.compose.InternalLazyColumnScrollbar
+import my.nanihadesuka.compose.LazyColumnScrollbar
+import my.nanihadesuka.compose.ScrollbarSelectionMode
 
 @Composable
 internal fun About(
@@ -39,9 +47,22 @@ internal fun About(
             }
         },
     ) { contentPadding ->
+        val listState = rememberLazyListState()
         LibrariesContainer(
             modifier = Modifier.fillMaxSize(),
             contentPadding = contentPadding,
+            lazyListState = listState,
         )
+        Box(modifier = Modifier
+            .padding(contentPadding)
+        ) {
+            InternalLazyColumnScrollbar(
+                listState = listState,
+                thickness = 8.dp,
+                selectionMode = ScrollbarSelectionMode.Thumb,
+                thumbColor = MaterialTheme.colors.primary.copy(alpha = 0.7f),
+                thumbSelectedColor = MaterialTheme.colors.primary,
+            )
+        }
     }
 }
