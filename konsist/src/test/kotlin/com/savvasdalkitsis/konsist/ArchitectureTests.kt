c9f0319cf5dabfaa4032bed8c3440afb6c59f5a7
Savvas Dalkitsis <kurosavvas@gmail.com>
2024-09-21 12:48:40 +0100

    Adding first architecture test but ignored until all issues fixed

--- konsist/src/test/kotlin/com/savvasdalkitsis/konsist/ArchitectureTests.kt
+++ konsist/src/test/kotlin/com/savvasdalkitsis/konsist/ArchitectureTests.kt
@@ -1,8 +1,11 @@
 package com.savvasdalkitsis.konsist
 
 import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.architecture.KoArchitectureCreator.assertArchitecture
+import com.lemonappdev.konsist.api.architecture.Layer
 import com.lemonappdev.konsist.api.ext.list.withNameEndingWith
 import com.lemonappdev.konsist.api.verify.assertTrue
+import org.junit.Ignore
 import org.junit.Test
 
 class ArchitectureTests {
@@ -21,5 +24,16 @@ class ArchitectureTests {
             .classesAndInterfaces()
             .withNameEndingWith("Repository")
             .assertTrue { it.resideInPackage("..repository..") }
+    }
+
+    @Test
+    @Ignore("This is violated a lot so will be used to slowly fix the issues")
+    fun `UseCases should not use database layer classes directly`() {
+        Konsist.scopeFromProject()
+            .assertArchitecture {
+                val useCases = Layer("UseCases", "..usecase..")
+                val database = Layer("Database", "com.savvasdalkitsis.uhuruphotos.feature.db..")
+                useCases.doesNotDependOn(database)
+            }
     }
 }
